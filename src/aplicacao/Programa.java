ba245fcad5f7652c1b6d1c16cfc79de7b8c57fdb
Gilberto Silva <agilberto20111@hotmail.com>
2020-04-03 22:56:58 -0300

    Implementação do findById

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -2,18 +2,22 @@ package aplicacao;
 
 import java.util.Date;
 
+import modelo.dao.DaoFactory;
+import modelo.dao.VendedorDao;
 import modelo.entidade.Departamento;
 import modelo.entidade.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		 
-		Departamento obj = new Departamento(1,"Livros");
-		System.out.println(obj);
 		
-		Vendedor vendedor = new Vendedor(21, "Gilberto", "agilberto20111@hotmail.com", new Date(), 3000.0, obj);
-		System.out.println(vendedor);
+	VendedorDao vendedorDao = DaoFactory.createVendedorDao();
+	
+	Vendedor vendedor = vendedorDao.findById(3);
+			
+	System.out.println(vendedor);
+	
+		  
 	}
 
 }
--- src/modelo/dao/DaoFactory.java
+++ src/modelo/dao/DaoFactory.java
@@ -1,5 +1,14 @@
 package modelo.dao;
 
+import db.DB;
+import modelo.dao.impl.VendedorDaoJDBC;
+
 public class DaoFactory {
+	
+	public static VendedorDao createVendedorDao() {
+		
+		return new VendedorDaoJDBC(DB.getConnection());
+		
+	}
 
 }
